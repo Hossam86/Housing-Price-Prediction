29cde42cf671fb606a2d30d66117eb91019b2334
Hossam Samir <Hossamsamir@outlook.com>
2019-06-30 18:01:14 +0200

    load data set

--- RF_reg.py
+++ RF_reg.py
@@ -2,4 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
+PATH= "DataSets/housing/"
+os.environ['PATH']
+
+#use pandas to read in csv files . The pd,read_csv() method creates a DataFram fro a csv file
+train =pd.read_csv(f'{PATH}train.csv',low_memory=False)
+test= pd.read_csv(f'{PATH}test.csv',low_memory=False)
+
+#checkout  the size of the data
+print ("Train data shape:",train.shape)
+print("Test data shape:",test.shape)
+
+
+# look at a few rows using the DataFrame.head() method
+# train.head()
+print(train.head())
+
+#to do some plotting
+plt.style.use(style='ggplot')
+plt.rcParams['figure.figsize'] = (10, 6)
