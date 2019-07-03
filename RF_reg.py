e46a47ef773d809da5d5b058401c489e4aaee15b
Hossam Samir <Hossamsamir@outlook.com>
2019-07-03 16:23:57 +0200

    Sale price dist

--- RF_reg.py
+++ RF_reg.py
@@ -12,17 +12,17 @@ train =pd.read_csv(f'{PATH}train.csv',low_memory=False)
 test= pd.read_csv(f'{PATH}test.csv',low_memory=False)
 
 #checkout  the size of the data
-print ("Train data shape:",train.shape)
-print("Test data shape:",test.shape)
+# print ("Train data shape:",train.shape)
+# print("Test data shape:",test.shape)
 
 
 # look at a few rows using the DataFrame.head() method
 # train.head()
-print(train.head())
+# print(train.head())
 
 #to do some plotting
 plt.style.use(style='ggplot')
-plt.rcParams['figure.figsize'] = (10, 6)
+# plt.rcParams['figure.figsize'] = (10, 7)
 
 #######################################################
 #  2. Explore the data and engineer Features          
@@ -31,7 +31,7 @@ numeric_features = train.select_dtypes(include=[np.number])
 QFact=numeric_features.drop(['Id'],axis=1)
 # Side-by-side: skew and Kurtosis
 # ==============================================================
-fig=plt.figure(11,figsize=(12,4))
+fig=plt.figure(1,figsize=(16,8))
 title=fig.suptitle("Skewness and Kurtosis in data ", fontsize=16, fontweight='bold')
 fig.subplots_adjust(top=0.88,wspace=0.3)
 dataSk=QFact.skew()
@@ -40,7 +40,7 @@ dataKu=QFact.kurt()
 # skew #
 #===========#
 ax1=fig.add_subplot(1,2,1)
-ax1.set_xlabel("features")
+# ax1.set_xlabel("features")
 ax1.set_ylabel("Skewness")
 
 bars1 = ax1.bar(QFact.columns,dataSk,color='darksalmon',edgecolor='darkred', linewidth=1)
@@ -50,7 +50,7 @@ plt.xticks(rotation=90)
 # Kurtosis #
 #==============#
 ax2 = fig.add_subplot(1,2,2) 
-ax2.set_xlabel("features")
+# ax2.set_xlabel("features")
 ax2.set_ylabel("Kurtosis") 
 bars2 = ax2.bar(QFact.columns,dataKu,color='darksalmon',edgecolor='darkred', linewidth=1)
 plt.xticks(rotation=90)
@@ -59,4 +59,18 @@ ax2.tick_params(axis='both', which='major', labelsize=8)
 # Save Figure #
 #=============#
 fig.savefig('Housing-Price-Prediction/skew_kurt.png')
+plt.show()
+# ==============================================================================================
+# to plot a histogram of SalePrice
+print ("Skew is:", train.SalePrice.skew())
+plt.hist(train.SalePrice, color='blue')
+plt.show()
+sns.distplot(train['SalePrice'])
+plt.show()
+
+target = np.log(train.SalePrice)
+print ("\n Skew is:", target.skew())
+plt.hist(target, color='blue')
+plt.show()
+sns.distplot(target)
 plt.show()
